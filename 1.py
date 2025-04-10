42b36bdda5cbd6e39d96bd77bf6825b641d1f7cd
Taizoo Avila <riavila@unal.edu.co>
2025-04-10 18:26:42 -0500

    Código 1

---
+++ 1.py
@@ -0,0 +1,16 @@
+num1: float = float(input("Ingrese el primer número: "))
+num2: float = float(input("Ingrese el segundo número: "))
+operador: str = input("Ingrese el símbolo de la operación que quiere realizar (+, -, *, /)")
+lista_operadores = ("+", "-", "*", "/")
+while operador not in lista_operadores:
+    operador = input("inserte un operador válido (+, -, *, /)")
+def Operaciones (num1, num2, operador):
+    if operador == "+":
+        return num1 + num2
+    elif operador == "-":
+        return num1 - num2
+    elif operador == "*":
+        return num1 * num2
+    else:
+        return num1 / num2
+print(Operaciones(num1, num2, operador))
